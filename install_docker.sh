7258418dfd11bfc1251289ded5b9cdcd705094b0
Lizkes <erfan2333@gmail.com>
2020-09-06 21:48:51 +0800

    change to china souce

--- install_docker.sh
+++ install_docker.sh
@@ -9,10 +9,10 @@ apt-get -y install \
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   "deb [arch=amd64] https://docker.mirrors.ustc.edu.cn \
    $(lsb_release -cs) \
    stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
