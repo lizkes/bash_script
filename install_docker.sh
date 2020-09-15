47884506973075618a5c0440883a38b591373575
lizkes <erfan2333@gmail.com>
2020-09-15 23:41:51 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -9,7 +9,7 @@ apt-get -y install \
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
-   "deb [arch=amd64] https://docker.mirrors.ustc.edu.cn \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 apt-get update
