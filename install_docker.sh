1a4bf11f787b5a3508bb93bff3e32c26c8c74054
lizkes <erfan2333@gmail.com>
2021-01-21 19:33:10 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -9,9 +9,7 @@ apt-get -y install \
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+   "deb [arch=amd64] https://download.docker.com/linux/debian buster stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
